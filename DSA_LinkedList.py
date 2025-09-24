146e87bd397eefb04c0517cec6e88583b3ac7a45
Ameya Bodhankar <ameyabodhankar@192.168.1.4>
2025-09-25 02:18:09 +0530

    [EDA][DSA] Practice codes

---
+++ DSA_LinkedList.py
@@ -0,0 +1,11 @@
+class ssl:
+    def __init__(self, start_node=None):
+        self.start_node = start_node
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+
